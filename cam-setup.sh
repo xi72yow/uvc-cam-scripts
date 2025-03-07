a1019e453512f7a2407b67aaa3a5a734e81af4be
xi72yow <65042627+xi72yow@users.noreply.github.com>
2025-03-07 18:16:21 +0100

    Update cam-setup.sh

--- cam-setup.sh
+++ cam-setup.sh
@@ -2,3 +2,5 @@
 v4l2-ctl -d /dev/video0 --set-ctrl=white_balance_automatic=0 &&
 v4l2-ctl -d /dev/video0 --set-ctrl=white_balance_temperature=3000 &&
 v4l2-ctl -d /dev/video0 --set-ctrl=zoom_absolute=36
+
+# sudo apt install v4l-utils
